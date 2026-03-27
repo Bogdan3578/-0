48f2f225ae717d7e6cfd5269909f6d0b88a30265
Bogdan3578 <alekseevbogdann@yandex.ru>
2026-03-27 17:28:26 +0300

    Add files via upload

---
+++ Оригенал вывода массивов.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+#include <windows.h>
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const int size{ 10 };
+    int chislo[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << chislo[i] << ", ";
+    }
+}
+
